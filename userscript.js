900969cc314169a8a3de2fc1f07daf2f29a815a1
yhx-12243 <yhx0706@163.com>
2021-10-16 13:28:49 +0800

    1.1

--- userscript.js
+++ userscript.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ioihw2021 做题工具
-// @version      1.0
+// @version      1.1
 // @author       yhx-12243
 // @match        https://ioihw21.duck-ac.cn/
 // @match        https://ioihw21.duck-ac.cn/*
@@ -110,6 +110,10 @@ const colors = [
 	'red',
 ];
 
+const vains = [
+	4
+];
+
 const db = {
 	load() {
 		return JSON.parse(localStorage.getItem('hw') || '[]');
@@ -132,12 +136,8 @@ const dbWinner = {
 		localStorage.setItem('hw-winner', String(winner));
 	},
 	query() {
-		let plain = localStorage.getItem('hw-winner');
-		if (isNaN(parseInt(plain))) {
-			return -1;
-		} else {
-			return parseInt(plain);
-		}
+		let plain = parseInt(localStorage.getItem('hw-winner'));
+		return isNaN(plain) ? -1 : plain;
 	}
 };
 
@@ -151,10 +151,7 @@ function getProblemInfo(problemId) {
 		authorName = 'root';
 	}
 
-	return {
-		problemType,
-		authorName
-	};
+	return {problemType, authorName};
 }
 
 function getUserInfo(id) {
@@ -163,27 +160,20 @@ function getUserInfo(id) {
 		if (match_result) {
 			return match_result[1];
 		} else {
-			console.log("[Warning] string doesn't match!");
+			console.warn(`[Warning] string doesn't match! (${source}, ${reg_exp})`);
 			return default_value;
 		}
 	}
 	id = id.toString().padStart(2, '0');
-	return $.get({
-		url: `/user/profile/ioi2022_${id}`,
-	}).then((res) => {
+	return fetch(`/user/profile/ioi2022_${id}`).then(res => res.text()).then(res => {
 		let motto = strMatch(res, /<h4 class="list-group-item-heading">格言<\/h4>\s+<p class="list-group-item-text">(.*?)<\/p>/s, '<div class="text-danger">&lt;error&gt;</div>');
-		let regex = /"\/problem\/(\d+)"/g, match, count = 0;
-		while (match = regex.exec(res)) {
+		let count = 0;
+		for (let match of res.matchAll(/"\/problem\/(\d+)"/g)) {
 			let problemId = parseInt(match[1]);
-			let {problemType} = getProblemInfo(problemId);
-			count += problemType == '互测题';
+			count += getProblemInfo(problemId).problemType === '互测题';
 		}
 
-		return {
-			id,
-			motto,
-			count,
-		};
+		return {id, motto, count};
 	});
 }
 
@@ -193,11 +183,10 @@ async function render() {
 		if (match = this.textContent.match(/^ioi2022_([0-9]+)$/)) {
 			let uid = parseInt(match[1]);
 			let name = userlist[uid];
-			if (uid == dbWinner.query()) {
+			if (uid === dbWinner.query()) {
 				name += '<sup style="color: red">卷王</sup>';
 			}
 			if (name) {
-				console.log(uid, name);
 				this.innerHTML = '<span style="font-weight: normal">' + name + '</span>';
 			}
 		}
@@ -213,62 +202,8 @@ async function render() {
 			} else {
 				this.style['font-weight'] = 'normal';
 			}
-			console.log(pid, status);
 		});
 	}
-}
-
-async function mainRender() {
-	$('.navbar .navbar-nav').append('<li><a href="/ranklist">排行榜</a></li>')
-
-	if (location.pathname == '/ranklist') {
-		document.title = document.title.replace('比赛排行榜', '排行榜');
-
-		let userListPromised = [];
-		$('.pagination').remove();
-		$('.table tbody tr').remove();
-		for (let userId = 0; userId < userlist.length; userId++) {
-			userListPromised.push(getUserInfo(userId));
-		}
-		let userList = await Promise.all(userListPromised);
-		userList.sort((firstUser, secondUser) => {
-			console.log(firstUser, secondUser);
-			if (firstUser.count == secondUser.count) {
-				return parseInt(firstUser.id) - parseInt(secondUser.id);
-			}
-			return secondUser.count - firstUser.count;
-		});
-		if (userList.length) {
-			dbWinner.update(userList[0].id);
-		}
-		console.log(userList);
-		$('.table thead tr th:last-child').text('通过数');
-		for (let user of userList) {
-			console.log(user);
-			let $tr = $('<tr></tr>');
-			$tr.append(`<td>${user.id}</td>`);
-			$tr.append(`<td><a class="uoj-username" href="/user/profile/ioi2022_${user.id}" style="color: #4bafb2">ioi2022_${user.id}</a></td>`);
-			$tr.append(`<td>${user.motto}</td>`);
-			$tr.append(`<td>${user.count}</td>`);
-			$('.table tbody').append($tr);
-		}
-	}
-
-	let match;
-	if (match = location.pathname.match(/\/problem\/(\d+)/)) {
-		let problemId = parseInt(match[1]);
-		let {problemType, authorName} = getProblemInfo(problemId);
-
-		$('.nav-tabs').eq(0).append(`<li>
-	        <span style="display:block;padding:10px 15px;font-weight:bold;">
-	            ${problemType}
-	            <span style="font-weight:normal">by</span>
-	            <a class="uoj-username" target="_blank" href="/user/profile/${authorName}" style="color: #4bafb2">${authorName}</a>
-	        </span>
-	    </li>`);
-	}
-
-	render();
 }
 
 if (location.pathname.startsWith('/problems')) {
@@ -281,4 +216,267 @@ if (location.pathname.startsWith('/problems')) {
 	});
 }
 
-mainRender();
+class Ranklist {
+	static async render() {
+		document.title = document.title.replace('比赛排行榜', '卷王榜');
+
+		let userListPromised = [];
+		$('.pagination').remove();
+		$('.table tbody tr').remove();
+		for (let userId = 0; userId < userlist.length; userId++) {
+			userListPromised.push(getUserInfo(userId));
+		}
+		let userList = await Promise.all(userListPromised);
+		userList.sort((firstUser, secondUser) => {
+			if (firstUser.count === secondUser.count) {
+				return parseInt(firstUser.id) - parseInt(secondUser.id);
+			}
+			return secondUser.count - firstUser.count;
+		});
+		if (userList.length) {
+			dbWinner.update(userList[0].id);
+		}
+		$('.table thead tr th:last-child').text('通过数');
+		for (let user of userList) {
+			let $tr = $('<tr></tr>');
+			$tr.append(`<td>${user.id}</td>`);
+			$tr.append(`<td>${getUserLink(`ioi2022_${user.id}`, 1500)}</td>`);
+			$tr.append(`<td>${user.motto}</td>`);
+			$tr.append(`<td>${user.count}</td>`);
+			$('.table tbody').append($tr);
+		}
+	}
+}
+
+class mainRanklist {
+	static analyze() {
+		this.standings = userlist.map((name, idx) => ({
+			idx,
+			id: `ioi2022_${idx.toString().padStart(2, '0')}`,
+			points: 0, // limit 100
+			points_S60: 0, // limit 80
+			points_F20: 0, // limit 20
+			records: this.contests.map(contest => ({contest}))
+		}));
+		let standings_map = {}, CTT = this.standings.slice(0, 50).map(x => x.id);
+		this.standings.forEach(standing => standings_map[standing.id] = standing);
+		globalThis.asdf = this.contests;
+		this.contests.forEach((contest, idx) => {
+			let i, j = -1, c = 0, restrict = Infinity, st = contest.data.standings, sc = contest.data.score;
+			for (i = 0; i < st.length; ++i)
+				if (CTT.includes(st[i][2][0])) {
+					if (++c <= 20) restrict = st[i][0];
+					st[i].push(~j && st[i][0] === st[j][0] ? st[j][4] : c);
+					j = i;
+				}
+			contest.first20 = restrict;
+			st.forEach(row => {
+				let id = row[2][0], user = standings_map[id], record = user.records[idx];
+				[record.score, , , record.rank, record.ctt_rank] = row;
+				record.points_S60 = (record.score >= 60 ? 10 : 0);
+				record.points_F20 = (record.score >= contest.first20 ? 3 : 0);
+				user.points_S60 += record.points_S60;
+				user.points_F20 += record.points_F20;
+			});
+			for (let id in sc) {
+				let user = standings_map[id];
+				user.records[idx].details = sc[id];
+			}
+		});
+	}
+
+	static render($container, header) {
+		$('head').append(`<style>
+.table>tbody>tr>td.warning {background-color: #fcf8e3 !important}
+.table>tbody>tr:hover>td.warning {background-color: #faf2cc !important}
+.table>tbody>tr>td.success {background-color: #dff0d8 !important}
+.table>tbody>tr:hover>td.success {background-color: #d0e9c6 !important}
+</style>`);
+		$container.long_table(this.standings, 1, header, (row, idx) => {
+			let ret = (row.idx >= 50 ? '<tr class="info">' : '<tr>');
+			ret += `<td>${row.idx.toString().padStart(2, '0')}</td><td>${getUserLink(row.id, 1500)}</td>`;
+			for (let record of row.records) {
+				let n = record.contest.data.problems.length, rank_str = '';
+				if (record.rank) {
+					rank_str = `${record.ctt_rank ? record.ctt_rank : '-'} (${record.rank})`;
+				}
+				ret += `<td${(record.score ?? -1) >= record.contest.first20 ? ' class="warning"' : ''}>${rank_str}</td>`;
+				record.details ??= [];
+				for (let i = 0; i < n; ++i) {
+					if (record.details[i]) {
+						let [score, , sid] = record.details[i];
+						ret += `<td><a href="/submission/${sid}" class="uoj-score" style="color: ${getColOfScore(score)}">${score}</a></td>`;
+					} else {
+						record.details[i] = [-1];
+						ret += '<td></td>';
+					}
+				}
+				if (record.score != undefined) {
+					ret += `<td${record.score >= 60 ? ' class="success"' : ''}><span class="uoj-score" data-max="${n * 100}" style="color: ${getColOfScore(record.score / n)}">${record.score}</span></td>"`;
+				} else {
+					record.score = -1;
+					ret += '<td></td>';
+				}
+				record.points = (record.points_S60 ??= 0) + (record.points_F20 ??= 0);
+				ret += `<td>${(record.points * .05).toFixed(2)} (${(record.points_S60 * .05).toFixed(2)} + ${(record.points_F20 * .05).toFixed(2)})</td>`;
+			}
+			row.points = Math.min(row.points_S60, 80) + Math.min(row.points_F20, 20);
+			ret += `<td>${(row.points * .05).toFixed(2)} (${(Math.min(row.points_S60, 80) * .05).toFixed(2)} + ${(Math.min(row.points_F20, 20) * .05).toFixed(2)})</td></tr>`;
+			return ret;
+		}, {
+			echo_full: true,
+			get_row_index: true,
+			page_len: 1e308,
+			table_classes: ['table', 'table-bordered', 'table-hover', 'table-striped', 'table-text-center', 'table-vertical-middle', 'table-condensed']
+		}).prepend('<div class="text-right text-muted">提示：点击表头可以排序</div>');
+
+		let rows = $container.find('thead>tr').get(),
+			body = $container.find('tbody'),
+			data_rows = [...body.get(0).rows];
+		console.assert(data_rows.length === this.standings.length);
+		for (let i = 0; i < this.standings.length; ++i) data_rows[i].extra_ioi2022 = this.standings[i];
+		// compare functions
+		const cmp = (A, B) => (A < B ? -1 : A > B ? 1 : 0),
+			cmpIdx = (A, B) => cmp(A.idx, B.idx),
+			andThen = (f, g) => (A, B) => f(A, B) || g(A, B),
+			sort = f => {
+				data_rows.sort((A, B) => f(A.extra_ioi2022, B.extra_ioi2022));
+				body.empty().append(data_rows);
+			},
+			cmpFns = {
+				natural: cmpIdx,
+				username: andThen((A, B) => cmp(userlist[A.idx], userlist[B.idx]), cmpIdx),
+				points: andThen((A, B) => -cmp(A.points, B.points), cmpIdx),
+				details: []
+			};
+
+		this.contests.forEach((contest, idx) => {
+			let n = contest.data.problems.length;
+			let cmpScore = andThen((A, B) => -cmp(A.records[idx].score, B.records[idx].score), cmpIdx);
+			cmpFns.details.push(cmpScore);
+			for (let i = 0; i < n; ++i)
+				cmpFns.details.push(andThen((A, B) => -cmp(A.records[idx].details[i][0], B.records[idx].details[i][0]), cmpIdx));
+			cmpFns.details.push(cmpScore, cmpScore);
+		});
+
+		$container.find('th').click(function (e) {
+			let tr = this.parentNode, idx = rows.indexOf(tr), idc = [].indexOf.call(tr.cells, this);
+			if (idx === 0) {
+				if (idc === 0) return sort(cmpFns.natural);
+				if (idc === 1) return sort(cmpFns.username);
+				if (idc === tr.cells.length - 1) return sort(cmpFns.points);
+			} else if (idx === 1) {
+				return sort(cmpFns.details[idc]);
+			} else {
+				console.warn('[Warning] Unknown element', this);
+			}
+		});
+	}
+
+	static async getContest(contest) {
+		// load data if immutable
+		try {
+			let res = localStorage.getItem(`contest_${contest.id}`);
+			if (!res) throw Error;
+			contest.data = JSON.parse(res);
+			return;
+		} catch (e) {
+		};
+		// fetch data
+		let response = await (await fetch(`/contest/${contest.id}/standings`)).text();
+		let raw = response.match(/<div id="standings"><\/div><script type="text\/javascript">(.*?)<\/script>/s);
+		if (!raw) {
+			contest.data = 'failed';
+			return;
+		}
+		raw = raw[1];
+		raw = raw.slice(0, raw.lastIndexOf('$'));
+		contest.data = (r => {
+			let standings_version, contest_id, standings, score, problems;
+			eval(r);
+			return {standings_version, contest_id, standings, score, problems};
+		})(raw);
+		// save data
+		localStorage.setItem(`contest_${contest.id}`, JSON.stringify(contest.data));
+	}
+
+	static async main() {
+		document.title = document.title.replace('404', '互测总榜');
+		let $container = $('div.uoj-content').empty();
+		let response = await (await fetch('/contests')).text();
+		this.contests = [];
+		for (let match of response.matchAll(/<tr><td><a.*?<\/tr>/gs)) {
+			let $cell = $.parseHTML(match[0])[0].cells[0];
+			let $a = $cell.querySelector('a');
+			let id = +$a.href.slice($a.href.lastIndexOf('/') + 1);
+			if (!isNaN(id) && !vains.includes(id) && $cell.querySelector('sup').textContent === '已结束') {
+				this.contests.push({id, name: $a.textContent});
+			}
+		}
+		let promises = this.contests.map(this.getContest);
+		await Promise.all(promises);
+		this.contests = this.contests.sort((x, y) => x.id - y.id).filter(contest => contest.data !== 'failed');
+
+		let row_1 = ['<tr><th rowspan="2" style="min-width: 3em">#</th><th rowspan="2" style="min-width: 5em">用户名</th>'], row_2 = [''];
+		this.contests.forEach(contest => {
+			let n = contest.data.problems.length;
+			row_1.push(`<th colspan="${n + 3}" style="border-bottom-width: 1px; min-width: ${2 * n + 11.5}em"><a href="/contest/${contest.id}">${contest.name}</a></th>`);
+			row_2.push(
+				'<th style="min-width: 3.5em">排名</th>',
+				...contest.data.problems.map((problem, idx) => `<th style="min-width: 2em"><a href="/contest/${contest.id}/problem/${problem}">${String.fromCharCode(65 + idx)}</a></th>`),
+				'<th style="min-width: 3.5em">总分</th>',
+				'<th style="min-width: 4.5em">折算分</th>'
+			);
+		});
+		row_1.push('<th rowspan="2" style="min-width: 6em">总折算分</th>', '</tr>'), row_2.push('</tr>');
+
+		this.analyze();
+		this.render($container, row_1.concat(row_2).join(''));
+	}
+};
+
+(async () => {
+	$('.navbar .navbar-nav').append('<li><a href="/ranklist">卷王榜</a></li>')
+	$('.navbar .navbar-nav').append('<li><a href="/standings">互测总榜</a></li>')
+
+	if (location.pathname === '/ranklist') {
+		await Ranklist.render();
+	}
+
+	if (location.pathname === '/standings') {
+		await mainRanklist.main();
+	}
+
+	if (location.pathname.startsWith('/problems')) {
+		$('.table thead tr th:last-child').css('width', '170px');
+		$('.table thead tr').eq(0).append('<th class="text-center" style="width: 95px;">来源</th>');
+		$('.table tbody tr').each(function (index, element) {
+			let $element = $(element);
+			let problemId = $element.children('td').eq(0).text().slice(1);
+			let {problemType, authorName} = getProblemInfo(problemId);
+
+			let extraContent = '';
+			if (problemType === '互测题') {
+				extraContent = `by ${getUserLink(authorName, 1500)}`;
+			}
+
+			$element.append('<td>' + extraContent + '</td>');
+		});
+	}
+
+	let match;
+	if (match = location.pathname.match(/\/problem\/(\d+)/)) {
+		let problemId = parseInt(match[1]);
+		let {problemType, authorName} = getProblemInfo(problemId);
+
+		$('.nav-tabs').eq(0).append(`<li>
+			<span style="display: block; padding: 10px 15px; font-weight: bold">
+				${problemType}
+				<span style="font-weight: normal">by</span>
+				${getUserLink(authorName, 1500)}
+			</span>
+		</li>`);
+	}
+
+	render();
+})();
